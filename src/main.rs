b077c88615e47b0957e15c2fe9167b42a3ac7186
IanMaguithi <maguithian13427@gmail.com>
2023-05-15 15:22:33 +0300

    infinite loop

--- src/main.rs
+++ src/main.rs
@@ -10,22 +10,25 @@ fn main() {
     // Generate a random number between 1 and 100
     let secret_number = rand::thread_rng().gen_range(1..=100);
 
-    println!("Please input your guess.");
+    loop {
+        println!("Please input your guess.");
 
-    // Create a mutable variable that is currently bound to a new, empty instance of a String
-    let mut guess = String::new();
+        // Create a mutable variable that is currently bound to a new, empty instance of a String
+        let mut guess = String::new();
 
-    // Read input from user
-    io::stdin().read_line(&mut guess)
-        .expect("Failed to read line");
+        // Read input from user
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("Failed to read line");
 
-    let guess: u32 = guess.trim().parse().expect("Please type a number!");
+        let guess: u32 = guess.trim().parse().expect("Please type a number!");
 
-    println!("You guessed: {}", guess);
-    println!("The secret number is: {}", secret_number);
-    match guess.cmp(&secret_number) {
-        std::cmp::Ordering::Less => println!("Too small!"),
-        std::cmp::Ordering::Greater => println!("Too big!"),
-        std::cmp::Ordering::Equal => println!("You win!"),
+        println!("You guessed: {}", guess);
+        println!("The secret number is: {}", secret_number);
+        match guess.cmp(&secret_number) {
+            std::cmp::Ordering::Less => println!("Too small!"),
+            std::cmp::Ordering::Greater => println!("Too big!"),
+            std::cmp::Ordering::Equal => println!("You win!"),
+        }
     }
 }
